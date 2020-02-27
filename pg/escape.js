4d261bf459c09f3d8283605be3c13d12fa6fc540
Patrick Pissurno <contato@patrickpissurno.com.br>
2020-02-27 19:41:20 -0300

    Fixed issue #2
    The following characters shall not be escaped in PostgreSQL:
    \f
    \n
    \r
    \t
    \v

--- pg/escape.js
+++ pg/escape.js
@@ -6,9 +6,20 @@ module.exports = function (value){
 
     if(typeof(value) === 'string'){ // postgres strings are escaped with double quotes instead of backslash
         value = value.replace(/\\/gi, '\u{0004}');
+        value = value.replace(/\f/gi, '\u{0005}');
+        value = value.replace(/\n/gi, '\u{0007}');
+        value = value.replace(/\r/gi, '\u{0011}');
+        value = value.replace(/\t/gi, '\u{0012}');
+        value = value.replace(/\v/gi, '\u{0013}');
         value = sqlstring.escape(value);
         value = value.replace(/\\'/g, "''");
-        return value.replace(/\u{0004}/giu, '\\');
+        value = value.replace(/\u{0013}/giu, '\v');
+        value = value.replace(/\u{0012}/giu, '\t');
+        value = value.replace(/\u{0011}/giu, '\r');
+        value = value.replace(/\u{0007}/giu, '\n');
+        value = value.replace(/\u{0005}/giu, '\f');
+        value = value.replace(/\u{0004}/giu, '\\');
+        return value;
     }
 
     return sqlstring.escape(value);
--- pg/escape.test.js
+++ pg/escape.test.js
@@ -8,3 +8,8 @@ tap.equal(escape(`now()`), `now()`, 'now() should not be escaped');
 tap.equal(escape(`NOW()`), `NOW()`, 'NOW() should not be escaped');
 tap.equal(escape(1.32), sqlstring.escape(1.32), 'non-string values should be passed directly to sqlstring');
 tap.equal(escape("A \\ A"), `'A \\ A'`, 'should match');
+tap.equal(escape('some text\fmore text'), `'some text\fmore text'`, '\\f should not be escaped');
+tap.equal(escape('some text\nmore text'), `'some text\nmore text'`, '\\n should not be escaped');
+tap.equal(escape('some text\rmore text'), `'some text\rmore text'`, '\\r should not be escaped');
+tap.equal(escape('some text\tmore text'), `'some text\tmore text'`, '\\t should not be escaped');
+tap.equal(escape('some text\vmore text'), `'some text\vmore text'`, '\\v should not be escaped');
