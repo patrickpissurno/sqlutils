e6902bcf52628f5d30ce1f8f49a0599e8ce502e0
victormarquess11 <victormarquess11@gmail.com>
2024-05-20 17:22:04 -0300

    [FIX] path to type files

--- mysql/index.d.ts
+++ mysql/index.d.ts
@@ -1,23 +1,3 @@
-declare module 'sqlutils/pg' {
-    type Transformation = {
-        key: string | [string, string],
-        columns?: Array<string | [string, string]>,
-        children?: Array<Transformation & ChildTransformation> 
-    }
-
-    type ChildTransformation = {
-        rename: string,
-        single?: boolean,
-        flat?: boolean
-    }
-
-    export function format(statement: string, obj: object|object[]): string
-    export function escape(value: any): string
-    export function buildWhereFromQuery(query: object): string
-    export function groupColumnsToObjects(rows: object[], primary_key: string, groups: object[]): object[]
-    export function transformer(rows: any[], transformation: Transformation): object[]
-};
-
 declare module 'sqlutils/mysql' {
     type Transformation = {
         key: string | [string, string],
