a88c676f5df23d3d6e789b33be476a26bba6ce1d
Patrick Pissurno <patrick@pissurno.dev>
2025-10-15 21:56:15 -0300

    prettier

--- utils/recursiveMapsToLists.test.js
+++ utils/recursiveMapsToLists.test.js
@@ -3,70 +3,71 @@ const assert = require('node:assert');
 const recursiveMapsToLists = require('./recursiveMapsToLists');
 
 describe('recursiveMapsToLists', () => {
-    it('should be the same (1) - basic should work', () => {
-        const input = new Map();
-        input.set('a', {});
+  it('should be the same (1) - basic should work', () => {
+    const input = new Map();
+    input.set('a', {});
 
-        const found = recursiveMapsToLists(input);
+    const found = recursiveMapsToLists(input);
 
-        const wanted = [{}];
+    const wanted = [{}];
 
-        assert.deepStrictEqual(found, wanted);
-    });
-    
-    it('should be the same (2) - non objects should be kept as is', () => {
-        const input = new Map();
-        input.set('a', 'b');
+    assert.deepStrictEqual(found, wanted);
+  });
 
-        const found = recursiveMapsToLists(input);
+  it('should be the same (2) - non objects should be kept as is', () => {
+    const input = new Map();
+    input.set('a', 'b');
 
-        const wanted = ['b'];
+    const found = recursiveMapsToLists(input);
 
-        assert.deepStrictEqual(found, wanted);
-    });
+    const wanted = ['b'];
 
-    it('should be the same (3) - directly nested maps should work', () => {
-        const input = new Map();
-        input.set('a', new Map());
-        input.get('a').set('b', {});
+    assert.deepStrictEqual(found, wanted);
+  });
 
-        const found = recursiveMapsToLists(input);
+  it('should be the same (3) - directly nested maps should work', () => {
+    const input = new Map();
+    input.set('a', new Map());
+    input.get('a').set('b', {});
 
-        const wanted = [[{}]];
+    const found = recursiveMapsToLists(input);
 
-        assert.deepStrictEqual(found, wanted);
-    });
+    const wanted = [[{}]];
 
-    it('should be the same (4) - objects containing maps should work', () => {
-        const input = new Map();
-        input.set('a', { b: new Map() });
-        input.get('a').b.set('c', {});
+    assert.deepStrictEqual(found, wanted);
+  });
 
-        const found = recursiveMapsToLists(input);
+  it('should be the same (4) - objects containing maps should work', () => {
+    const input = new Map();
+    input.set('a', { b: new Map() });
+    input.get('a').b.set('c', {});
 
-        const wanted = [{ b: [{}] }];
+    const found = recursiveMapsToLists(input);
 
-        assert.deepStrictEqual(found, wanted);
-    });
+    const wanted = [{ b: [{}] }];
 
-    it('should be the same (5) - lists should be left as is', () => {
-        const input = new Map();
-        input.set('a', []);
+    assert.deepStrictEqual(found, wanted);
+  });
 
-        const found = recursiveMapsToLists(input);
+  it('should be the same (5) - lists should be left as is', () => {
+    const input = new Map();
+    input.set('a', []);
 
-        const wanted = [[]];
+    const found = recursiveMapsToLists(input);
 
-        assert.deepStrictEqual(found, wanted);
-    });
-    it('should be the same (6) - objects containing non map props should be left as is', () => {
-        const input = new Map();
-        input.set('a', { b: 'c' });
+    const wanted = [[]];
 
-        const found = recursiveMapsToLists(input);
+    assert.deepStrictEqual(found, wanted);
+  });
 
-        const wanted = [{ b: 'c' }];
+  it('should be the same (6) - objects containing non map props should be left as is', () => {
+    const input = new Map();
+    input.set('a', { b: 'c' });
 
-        assert.deepStrictEqual(found, wanted);
-    });
+    const found = recursiveMapsToLists(input);
+
+    const wanted = [{ b: 'c' }];
+
+    assert.deepStrictEqual(found, wanted);
+  });
 });
