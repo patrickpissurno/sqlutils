73a74a4b5f350967a070739fd71dae77d7541b97
Patrick Pissurno <patrick@pissurno.dev>
2026-06-13 17:30:13 -0300

    wip

--- index.ts
+++
@@ -1,2 +0,0 @@
-export * as pg from './src/pg/index.ts';
-export * as mysql from './src/mysql/index.ts';
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+export * as pg from './pg/index.js';
+export * as mysql from './mysql/index.js';
